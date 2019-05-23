2a55bc3fe2a032d795a2ceedc6549f7e35676f6d
Kim Ha Young <ha012345@naver.com>
2019-05-23 12:51:51 +0900

    delete

--- untitled/app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic.base import TemplateView
-
-class MainPageView(TemplateView):
-    template_name='main.html'
