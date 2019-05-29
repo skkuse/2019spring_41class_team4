b126986579022d4b5d9f6218a242aa48dc22dab4
한성주 <43755100+hancheng1995@users.noreply.github.com>
2019-05-29 22:45:26 +0900

    modle 수정
    테이블에 저장해야 되는 정보에 대해 수정

--- untitled/app/models.py
+++ untitled/app/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class Board(models.Model):
     subject = models.CharField(max_length=50, blank=True)
     name = models.CharField(max_length=30, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now=True)
     content = models.TextField(max_length=500, blank=True)
-    view = models.IntegerField(default=0)
+    view = models.IntegerField(blank=True)
 
 class food(models.Model):
-    name = models.CharField(max_length=50)
-    username = models.CharField(max_length=30, blank=True)
-    body = models.CharField(max_length=1024)
-    date = models.DateTimeField(auto_created=True, auto_now_add=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=50,null=True)
+    body = models.TextField(max_length=1024,null=True)
+    date = models.DateTimeField(auto_created=True,auto_now_add=True,null=True)
+    price = models.IntegerField(default=0)
 
     def generate(self):
         self.food =""
