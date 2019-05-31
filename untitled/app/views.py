5ce3b523e2d197e03844fe2761cd42bc77a8e4cd
Kim Ha Young <ha012345@naver.com>
2019-05-31 23:18:40 +0900

    #14 리스트 페이징 구현

--- untitled/app/views.py
+++ untitled/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from .models import Board, Comment, food
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .form import FoodForm
 from django.http import HttpResponse
 # Create your views here.
@@ -44,9 +45,29 @@ def logout(request):
 
 def community(request):
     boards = Board.objects.order_by('-id')
-    context = {'boards': boards}
+    page = request.GET.get('page', 1)
+    paginator = Paginator(boards, 20)
+    try:
+        lines = paginator.page(page)
+    except PageNotAnInteger:
+        lines = paginator.page(1)
+    except EmptyPage:
+        lines = paginator.page(paginator.num_pages)
+    context = {'boards': lines}
     return render(request, 'community.html', context)
 
+def foodlist(request):
+    foodlist = food.objects.order_by('-date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(foodlist, 8)
+    try:
+        lines = paginator.page(page)
+    except PageNotAnInteger:
+        lines = paginator.page(1)
+    except EmptyPage:
+        lines = paginator.page(paginator.num_pages)
+    context = {'foodlist': lines}
+    return render(request, 'foodlist.html', context)
 
 def purchase(request):
     return render(request, 'foodlist.html')
@@ -81,11 +102,6 @@ def submit_post(request):
     else:
         return render(request, 'write_post.html')
 
-def foodlist(request):
-    foodlist = food.objects.order_by('-date')
-    context = {'foodlist': foodlist}
-    return render(request, 'foodlist.html', context)
-
 def view_food(request, food_id):
     fd = food.objects.get(pk=food_id)
     context = {'food': fd}
@@ -98,6 +114,9 @@ def comment_write(request, board_id):
         content = request.POST["content"]
         Comment.objects.create(post=post, comment_writer=user.username, comment_contents=content)
         return redirect('community_view', board_id=board_id)
+
+
+
 
 
 
